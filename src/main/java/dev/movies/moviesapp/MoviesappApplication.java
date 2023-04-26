73c991c6972fb224aef844f126ae4b7988a7791e
Thiru63 <100009930+Thiru63@users.noreply.github.com>
2023-04-27 00:29:52 +0530

    Initial commit

---
+++ src/main/java/dev/movies/moviesapp/MoviesappApplication.java
@@ -0,0 +1,35 @@
+package dev.movies.moviesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@SpringBootApplication
+
+public class MoviesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesappApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
+
+
+
+}
---
+++ src/test/java/dev/movies/moviesapp/MoviesappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.movies.moviesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
